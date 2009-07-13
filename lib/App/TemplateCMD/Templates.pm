5223222459f3fa649d0d3d5c1aabf51e55190f6d
Ivan Wills <ivan.wills@gmail.com>
2009-07-13 14:07:31 +1000

    Changed templates (and accompnying test) to not run spelling or pod-coverage tests by default

--- lib/App/TemplateCMD/Templates.pm
+++ lib/App/TemplateCMD/Templates.pm
@@ -1146,7 +1146,7 @@ __perl/test.pl__
 [% IF not module %][% module = 'module' %][% END -%]
 [% IF not obj    %][% obj    = 'obj'    %][% END -%]
 [% IF not tests  %][% tests  = 10       %][% END -%]
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 
 BEGIN { $ENV{TESTING} = 1 }
 
@@ -1255,6 +1255,7 @@ TODO: {
 module_boilerplate_ok('lib/[% file %].pm');
 
 __perl/test/critic.t__
+#!/usr/bin/perl
 
 use strict;
 use warnings;
@@ -1336,6 +1337,11 @@ use strict;
 use warnings;
 use Test::More;
 
+if ( not $ENV{TEST_AUTHOR} ) {
+    my $msg = 'Author test.  Set TEST_AUTHOR environment variable to a true value to run.';
+    plan( skip_all => $msg );
+}
+
 # Ensure a recent version of Test::Pod::Coverage
 my $min_tpc = 1.08;
 eval "use Test::Pod::Coverage $min_tpc";
@@ -1370,9 +1376,26 @@ use strict;
 use warnings;
 use Test::More;
 
+if ( not $ENV{TEST_AUTHOR} ) {
+    my $msg = 'Author test.  Set TEST_AUTHOR environment variable to a true value to run.';
+    plan( skip_all => $msg );
+}
+
+# check that Test::Spelling is installed
 eval { require Test::Spelling; Test::Spelling->import() };
 
-plan skip_all => "Test::Spelling required for testing POD coverage" if $@;
+# now check that the spell command is installed
+my $found;
+for my $dir ( split /:/, $ENV{PATH} ) {
+	next if !-d $dir;
+	next if !-x "$dir/spell";
+
+	$found = 1;
+	last;
+}
+
+plan skip_all => "Test::Spelling required for testing POD spelling" if $@;
+plan skip_all => "spell cmd required for testing POD spelling" if !$found;
 
 add_stopwords(qw//);
 all_pod_files_spelling_ok();
