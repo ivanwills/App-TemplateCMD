51a109746a0a418561f4217be626e99da52c3d9d
Ivan Wills <ivan.wills@gmail.com>
2011-07-23 06:07:05 +1000

    Increased version for next release

--- lib/App/TemplateCMD.pm
+++ lib/App/TemplateCMD.pm
@@ -22,7 +22,7 @@ use Template::Provider::FromDATA;
 use Data::Merger qw/merger/;
 use base qw/Exporter/;
 
-our $VERSION     = version->new('0.1.0');
+our $VERSION     = version->new('0.1.1');
 our @EXPORT_OK   = qw//;
 our %EXPORT_TAGS = ();
 
@@ -384,7 +384,7 @@ App::TemplateCMD - Sets up an interface to passing Template Toolkit templates
 
 =head1 VERSION
 
-This documentation refers to App::TemplateCMD version 0.1.0.
+This documentation refers to App::TemplateCMD version 0.1.1.
 
 =head1 SYNOPSIS
 
